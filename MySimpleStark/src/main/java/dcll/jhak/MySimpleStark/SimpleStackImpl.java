4b2acf608e666af92365f69e8c04e8a4f910fa34
hkj0126a <jeanluc.hak@gmail.com>
2014-02-28 09:25:50 +0100

    #3 a

---
+++ MySimpleStark/src/main/java/dcll/jhak/MySimpleStark/SimpleStackImpl.java
@@ -0,0 +1,28 @@
+package dcll.jhak.MySimpleStark;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+//réponds à l'exigence 3
+//Commentaire du commit -m "#3 ..."
+public class SimpleStackImpl implements SimpleStack {
+	Stack <Object> wrappedStack = new Stack<Object>();
+	
+	public boolean isEmpty(){
+		return wrappedStack.isEmpty();
+	}
+	
+	public int getSize() {
+		return wrappedStack.size();
+	}
+	
+	public void push(Item item) {
+		wrappedStack.push(item);
+	}
+	
+	public Item peek() throws EmptyStackException {
+		return (Item) wrappedStack.peek();
+	}
+	public Item pop() throws EmptyStackException {
+		return (Item) wrappedStack.pop();
+	}
+}
